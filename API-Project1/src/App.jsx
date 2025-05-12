8b9df766e6fb7d3be47b667231bd96a78d2fbfbc
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-05-12 14:01:48 -0400

    fetch movies

--- API-Project1/src/App.jsx
+++ API-Project1/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
+const API = import.meta.env.VITE_API_KEY;
+const type = "movie";
+console.log(API);
 function App() {
+  
+  const fetchData = () => {
+    try {
+      fetch(`http://www.omdbapi.com/?apikey=${API}&type=${type}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    } catch (error) {
+      console.log("Error fetching movies", error);
+      return error;
+    }
+  };
 
   return (
     <>
-    <h1>Movies</h1>
+      <h1>Movies</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
