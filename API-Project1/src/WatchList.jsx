d112bce9e3794991b7701e48c6a65fc785ede3ff
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-05-20 09:49:19 -0400

    useNavigate back button functionality

--- API-Project1/src/WatchList.jsx
+++ API-Project1/src/WatchList.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Watchlist = ({ watchlist }) => {
+  const navigate = useNavigate();
+
   console.log("The watchlist: ", watchlist);
   return (
     <div className="watchlist">
+      <button onClick={() => navigate(-1)}>Go back</button>
       {watchlist.map((movie) => (
         <div key={movie.imdbID}>
           <h3>{movie.Title}</h3>
