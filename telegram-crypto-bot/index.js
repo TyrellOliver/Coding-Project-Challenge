b8fe6954ecdf3c9a5243150e8f8749acebe2b333
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-05-28 16:55:02 -0400

    feat: bot can send messages to client

--- telegram-crypto-bot/index.js
+++ telegram-crypto-bot/index.js
@@ -4,3 +4,24 @@ const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  console.log(match);
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/start/, (msg, match) => {
+  const chatId = msg.chat.id;
+  console.log("The match", match);
+  console.log("The msg", msg);
+  bot.sendMessage(
+    chatId,
+    "👋 Welcome! Send /price BTC to get the current Bitcoin price."
+  );
+});
