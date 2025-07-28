1500d5ec62673e26a27eade8ef12b2891a18fed8
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-07-28 11:18:20 -0400

    connected server with socket.io

--- Chat-Application/backend/app.js
+++ Chat-Application/backend/app.js
@@ -1,9 +1,19 @@
-const express = require("express")
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
 
-app.get((req, res)=>{
-    res.send("Welcome to the home page")
-})
+app.use(express.json());
 
-module.exports = app;
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page");
+});
+
+io.on("connection", (socket) => {});
+
+// httpServer.listen(3000);
+
+module.exports = { app, httpServer };
--- Chat-Application/backend/server.js
+++ Chat-Application/backend/server.js
@@ -1,8 +1,12 @@
-const app = require("./app");
+const {app, httpServer} = require("./app");
 
 require("dotenv").config();
 const { PORT } = process.env;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on PORT ${PORT}`);
+// });
+
+httpServer.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
 });
