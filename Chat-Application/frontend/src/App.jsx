2548943571ef8894bf4dca61953e8307665e1fe5
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-07-28 15:55:23 -0400

    add state to show sent message

--- Chat-Application/frontend/src/App.jsx
+++ Chat-Application/frontend/src/App.jsx
@@ -5,24 +5,50 @@ const socket = io.connect("http://localhost:3025");
 
 function App() {
   const [message, setMessage] = useState("");
-  const sendMessage = () => {
+  const [sentMessages, setSentMessages] = useState([]);
+  const [receivedMessages, setReceivedMessages] = useState([]);
+
+  const handleOnClick = () => {
     socket.emit("send_message", { message: message });
+    setSentMessages((prev) => [...prev, message]);
   };
+
   const handleTextChange = (e) => {
     // console.log(e.target.value)
     setMessage(e.target.value);
   };
 
-  // console.log(socket);
-  console.log(message);
+  socket.on("recevied_message", (data) => {
+    console.log(data);
+    setReceivedMessages((prev) => [...prev, data.message]);
+    // socket.emit("sent_message",(data)=>{
+
+    // })
+  });
+
+  // console.log(message);
+  // console.log(sentMessages);
   return (
     <>
+      <div>
+        <p>Sent</p>
+        {sentMessages.map((message) => (
+          <p>{message}</p>
+        ))}
+      </div>
+      <div>
+        <p>Received</p>
+
+        {receivedMessages.map((message) => (
+          <p>{message}</p>
+        ))}
+      </div>
       <input
         type="text"
         placeholder="Type your message..."
         onChange={handleTextChange}
       />
-      <button type="submit" onClick={sendMessage}>
+      <button type="submit" onClick={handleOnClick}>
         Send
       </button>
     </>
