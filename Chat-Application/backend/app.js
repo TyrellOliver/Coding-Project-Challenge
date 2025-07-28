ea06f02c8225db94e64c178f0a89b5331968cca7
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-07-28 13:47:13 -0400

    import cors

--- Chat-Application/backend/app.js
+++ Chat-Application/backend/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require(cors);
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {});
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3025",
+  },
+});
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
