0f7a1b61a352254de518e4cf74750a39f618c2ce
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-07-28 14:10:23 -0400

    cors origin updated

--- Chat-Application/backend/app.js
+++ Chat-Application/backend/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const cors = require(cors);
+const cors = require("cors");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3025",
-  },
-});
 
 app.use(cors());
 app.use(express.json());
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to the home page");
