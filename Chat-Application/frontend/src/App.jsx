7beda1efd4b8caff6a3c25f36461ebd977da2300
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-08-14 11:41:10 -0400

    Add react router, siderbar component, and home page

--- Chat-Application/frontend/src/App.jsx
+++ Chat-Application/frontend/src/App.jsx
@@ -1,93 +1,18 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import io from "socket.io-client";
-const socket = io.connect("http://localhost:3025");
+
+import Home from "./pages/Home";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [sentMessages, setSentMessages] = useState([]);
-  const [receivedMessages, setReceivedMessages] = useState([]);
-  const [room, setRoom] = useState("");
-
-  const handleOnClick = () => {
-    socket.emit("send_message", { message, room });
-    setSentMessages((prev) => [...prev, message]);
-    setMessage("");
-  };
-
-  // const handleTextChange = (e) => {
-  //   setMessage(e.target.value);
-  //   setRoom(e.target.value);
-  // };
-
-  const joinRoom = () => {
-    socket.emit("join_room", room);
-    // setRoom("");
-  };
-
-  useEffect(() => {
-    socket.on("recevied_message", (data) => {
-      setReceivedMessages((prev) => [...prev, data.message]);
-    });
-    return () => {
-      socket.off("recevied_message");
-    };
-  }, [socket]);
-
-  console.log(room);
-
   return (
-    <div>
-      <div className="sidebar">
-        <p>sidebar</p>
-      </div>
-      <div className="message_area">
-        <div>
-          <p>Sent:</p>
-          {sentMessages.map((message, index) => (
-            <div key={index}>
-              <p>{message}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <p>Received:</p>
-
-          {receivedMessages.map((message, index) => (
-            <div key={index}>
-              <p>{message}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Message..."
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={(e) =>
-              e.key.toLowerCase() === "enter" ? handleOnClick() : ""
-            }
-          />
-          <button type="submit" onClick={handleOnClick}>
-            Send
-          </button>
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Room number..."
-            value={room}
-            onChange={(e) => setRoom(e.target.value)}
-            onKeyDown={(e) =>
-              e.key.toLowerCase() === "enter" ? joinRoom() : ""
-            }
-          />
-          <button type="submit" onClick={joinRoom}>
-            Send
-          </button>
-        </div>
-      </div>
+    <div className="app">
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ Chat-Application/frontend/src/pages/Home.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3025");
+
+const Home = () => {
+  const [message, setMessage] = useState("");
+  const [sentMessages, setSentMessages] = useState([]);
+  const [receivedMessages, setReceivedMessages] = useState([]);
+  const [room, setRoom] = useState("");
+
+  const handleOnClick = () => {
+    socket.emit("send_message", { message, room });
+    setSentMessages((prev) => [...prev, message]);
+    setMessage("");
+  };
+
+  // const handleTextChange = (e) => {
+  //   setMessage(e.target.value);
+  //   setRoom(e.target.value);
+  // };
+
+  const joinRoom = () => {
+    socket.emit("join_room", room);
+    // setRoom("");
+  };
+
+  useEffect(() => {
+    socket.on("recevied_message", (data) => {
+      setReceivedMessages((prev) => [...prev, data.message]);
+    });
+    return () => {
+      socket.off("recevied_message");
+    };
+  }, [socket]);
+
+  console.log(room);
+  return (
+    <div className="message_area">
+      <div>
+        <p>Sent:</p>
+        {sentMessages.map((message, index) => (
+          <div key={index}>
+            <p>{message}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <p>Received:</p>
+
+        {receivedMessages.map((message, index) => (
+          <div key={index}>
+            <p>{message}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) =>
+            e.key.toLowerCase() === "enter" ? handleOnClick() : ""
+          }
+        />
+        <button type="submit" onClick={handleOnClick}>
+          Send
+        </button>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Room number..."
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          onKeyDown={(e) => (e.key.toLowerCase() === "enter" ? joinRoom() : "")}
+        />
+        <button type="submit" onClick={joinRoom}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
